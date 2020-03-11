30cbbae10df4e3f88c979cf90ff1dda772f83369
chenyisheng <chenyisheng@uworks-gd.cc>
2020-03-11 13:43:55 +0800

    相册预览

--- src/views/myform/bd/meeting.vue
+++ src/views/myform/bd/meeting.vue
@@ -22,10 +22,38 @@
             <a-input />
         </a-form-item>
         <a-form-item :label="$t('formbd.meetingArea')">
-            <a-input />
+            <a-radio-group name="radioGroup" :defaultValue="1">
+                <a-radio :value="1">{{ $t("formbd.ed") }}</a-radio>
+                <a-radio :value="2">{{ $t("formbd.ee") }}</a-radio>
+                <a-radio :value="3">{{ $t("formbd.ef") }}</a-radio>
+            </a-radio-group>
+            <a-row>
+                <a-col :span="6"> <a-date-picker /></a-col>
+                <a-col :span="18"> <a-input /></a-col>
+            </a-row>
+
+            <a-upload-dragger
+                name="file"
+                :multiple="true"
+                action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                @change="handleChange"
+            >
+                <p class="ant-upload-drag-icon">
+                    <a-icon type="inbox" />
+                </p>
+                <p class="ant-upload-text">Click or drag file to this area to upload</p>
+                <p class="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other band
+                    files
+                </p>
+            </a-upload-dragger>
         </a-form-item>
         <a-form-item :label="$t('formbd.parInformation')">
-            <a-input />
+            <div class="flex-justify-content-space-between">
+                <a-input style="width:32%;"></a-input>
+                <a-input style="width:32%;"></a-input>
+                <a-input style="width:32%;"></a-input>
+            </div>
             <a-upload-dragger
                 name="file"
                 :multiple="true"
--- src/views/show/detail/photo.vue
+++ src/views/show/detail/photo.vue
@@ -1,18 +1,46 @@
 <template>
-    <div class="flex warp">
-        <img class="img" :src="item.url" v-for="item in pictures" :key="item.id" />
+    <div class="flex warp" v-viewer ref="img">
+        <a-row type="flex" :gutter="10">
+            <a-col :span="4" v-for="item in pictures" :key="item">
+                <img class="img" :src="item" @click="showImg" />
+            </a-col>
+        </a-row>
     </div>
 </template>
 
 <script>
+import "viewerjs/dist/viewer.css";
+import Viewer from "v-viewer";
+import Vue from "vue";
+Vue.use(Viewer);
 export default {
-    props: { pictures: Array }
+    // props: { pictures: Array },
+    data() {
+        return {
+            pictures: [
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg",
+                "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583908268484&di=b4ba2c516f369fe632b336565f8ddce8&imgtype=0&src=http%3A%2F%2Fimage.biaobaiju.com%2Fuploads%2F20190928%2F19%2F1569669753-jnlcKyEuMZ.jpg"
+            ]
+        };
+    },
+
+    methods: {
+        showImg() {
+            const viewer = this.$refs.img.$el.$viewer;
+            console.log("vie==>", this.$refs.img);
+            // viewer.show();
+        }
+    }
 };
 </script>
 
 <style lang="less" scoped>
 .img {
-    width: 200px;
+    width: 100%;
     height: 120px;
 }
 .warp {
