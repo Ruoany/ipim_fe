98ba56d5a29e6f66fad60bb49e206d52efff095e
Alps <chentaio@qq.com>
2020-03-12 13:47:19 +0800

    同步代碼

--- src/apis/request.js
+++ src/apis/request.js
@@ -9,7 +9,7 @@ const request = axios.create({
 request.interceptors.request.use(
     config => {
         config.headers = {
-            token: sessionStorage.getItem("token")
+            Authorization: sessionStorage.getItem("token")
         };
         return config;
     },
