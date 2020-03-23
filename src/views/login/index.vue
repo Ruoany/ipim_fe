62fab097ef2843eca330e87eae18540918c2092c
chenyisheng <chenyisheng@uworks-gd.cc>
2020-03-23 10:15:06 +0800

    登录

---
+++ src/views/login/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="all">
+        <div class="top-back"></div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="less" scoped>
+.all {
+    position: relative;
+    .top-back {
+        background: #282828;
+        height: 500px;
+        border-bottom: 5px solid #e51c23;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+    }
+}
+</style>
